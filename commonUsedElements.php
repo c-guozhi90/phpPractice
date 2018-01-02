6f83c32a244332e4d5f19692ec455f0d3b5b2cbb
c-guozhi90 <c.guozhi90@gmail.com>
2018-01-02 14:15:04 +0800

    main structure

---
+++ commonUsedElements.php
@@ -0,0 +1,13 @@
+<nav>
+  <ul>
+    <li><a href="#">home page</a></li>
+    <li><a href="#">others</a></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+  </ul>
+</nav>
---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+ ?>
+
+ <!DOCTYPE html>
+ <html>
+   <head>
+     <meta charset="utf-8">
+     <title>home page</title>
+     <link rel="stylesheet" href="css/style.css">
+   </head>
+   <body>
+<header>
+  <?php require_once "commonUsedElements.php"; ?>
+</header>
+<main>
+
+</main>
+<footer></footer>
+   </body>
+ </html>
